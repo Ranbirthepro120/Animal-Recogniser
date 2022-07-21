18d8fa2f0812f248f2d46f76e8d071169a0ca43f
Ranbirthepro120 <75839328+Ranbirthepro120@users.noreply.github.com>
2022-07-21 17:11:04 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+var dog = 0;
+var cat = 0;
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true, video:false});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json', { probabilityThreshold: 0.7 } ,modelReady);
+}
+function modelReady()
+{
+  classifier.classify(gotResults);
+}
+function gotResults(error, results)
+{
+  if (error)
+  {
+    console.error(error);
+  }
+  else
+  {
+    console.log(results);
+    document.getElementById("results_count").innerHTML = 'Detected Dog' + dog + '        Detected Cat' + cat;
+    document.getElementById("results_label").innerHTML = 'I can hear - ' + results[0].label;
+    if (results[0].label == "Barking")
+    {
+      img.src = 'bark.gif';
+      dog = dog + 1;
+    } else if (results[0].label == "Meowing")
+    {
+      img.src = 'meow.gif';
+      cat = cat + 1;
+    } else 
+    {
+      img.src = 'listen.gif';
+    }
+  }
+    
+   
+ 
+  
+  
+}
